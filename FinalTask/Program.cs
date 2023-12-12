d59869b770f74807c7ece7a0c9e871cc95fd8c89
Ilya Ovsenev <ovsenev.ilya@yandex.ru>
2023-12-12 17:07:36 +0300

    Добавьте файлы проекта.

---
+++ FinalTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathFile = @"C:\Users\ovsen\OneDrive\Рабочий стол\Students.dat";
+            List<Student> students = new List<Student>();
+            if (!File.Exists(pathFile))
+            {
+                Console.WriteLine("no file");
+                return;
+            }
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+
+
+            using (var reader = new FileStream(pathFile, FileMode.OpenOrCreate))
+            {
+                students.AddRange((Student[])binaryFormatter.Deserialize(reader));
+            }
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.Name + student.Group);
+            }
+        }
+    }
+}
---
+++ FinalTask/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    [Serializable]
+    internal class Student
+    {
+        public string Name;
+        public string Group;
+        public DateTime DateOfBirth;
+
+        public Student(string name, string group, DateTime dateOfBirth)
+        {
+            Name = name;
+            Group = group;
+            dateOfBirth = dateOfBirth;
+        }
+    }
+}
